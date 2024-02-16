cb3727b2d6437f6547551435ac09f20010021c4a
OG0808 <ogarns@proton.me>
2024-02-16 12:07:33 -0500

    config tailwind, dependencias reactrouter, flowbyte, react hook form

---
+++ front/src/routes/index.jsx
@@ -0,0 +1,21 @@
+import { createBrowserRouter } from "react-router-dom";
+
+
+
+
+export const router = createBrowserRouter ([
+     //  ubicar rutas aqui ejemplo:
+
+    // {
+    //     path: "/",
+    //     element: <Home />,
+    // },
+    // {
+    //     path: "/about",
+    //     element: <About />,
+    // },
+    // {
+    //     path: "/blog",
+    //     element: <Blog />,
+    // },
+])
