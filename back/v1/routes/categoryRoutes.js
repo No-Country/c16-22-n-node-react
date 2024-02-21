bdaa290ac65aeaa306bf1c75a804faa09361e7a6
martin-tercero1 <martintercero20@hotmail.com>
2024-02-20 20:33:13 -0600

    Add back the category and professional routes

---
+++ back/v1/routes/categoryRoutes.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const router = express.Router();
+
+const categoryController = require("../controllers/categoryController");
+
+router.get("/", categoryController.getAllCategories);
+
+router.get("/:categoryId", categoryController.getOneCategory);
+
+router.patch("/:categoryId", categoryController.updateOneCategory);
+
+router.post("/", categoryController.createNewCategory);
+
+router.delete("/:categoryId", categoryController.deleteOneCategory);
+
+module.exports = router;
