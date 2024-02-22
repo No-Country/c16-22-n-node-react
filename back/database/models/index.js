83d20a85a8da1c88a53f2d02473c3abdfacfc10f
OG0808 <113655962+OG0808@users.noreply.github.com>
2024-02-21 21:10:51 -0500

    Delete back directory

--- back/database/models/index.js
+++
@@ -1,9 +0,0 @@
-const models = {
-    categoryModel: require('./category'),
-    professionalModel: require('./professional'),
-    userModel: require('./user'),
-    messageModel: require('./message'),
-    chatModel: require('./chat'),
-}
-
-module.exports = models;
