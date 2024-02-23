64a1f4b73438d738463f3016a7134af3cc6f9af3
martin-tercero1 <martintercero20@hotmail.com>
2024-02-22 20:21:36 -0600

    Getting response after loggin in

--- front/src/pages/Home.jsx
+++ front/src/pages/Home.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import { useEffect } from 'react'
 import Header from '../components/header/Header'
 import Footer from '../components/footer/Footer'
 import Categories from '../components/categories/Categories'
+import { useNavigate } from 'react-router-dom'
 
 const Home = () => {
+  // If the user is already login we should redirect them to the desired page
+  const navigate = useNavigate()
+
+    // useEffect(() => {
+    //   const user = JSON.parse(localStorage.getItem("userInfo"));
+    //   if (user) {
+    //     navigate("/");
+    //   }
+    // });
+
   return (
     <div className=''>
      <Header/>
