72541bff3671c0f90d4c790f80fa0699cd69beb0
martin-tercero1 <martintercero20@hotmail.com>
2024-02-24 12:31:04 -0600

    Add tutorials section

---
+++ front/src/components/tutorials/Tutorials.jsx
@@ -0,0 +1,33 @@
+import { useEffect } from 'react'
+import VideoList from '../videolist/VideoList'
+import axios from 'axios'
+import { useState } from 'react'
+
+function Tutorials() {
+    const [videosData, setVideosData] = useState([]);
+
+    // useEffect( () => {
+    //     axios
+    //       .get(
+    //         "https://www.googleapis.com/youtube/v3/videos?key=AIzaSyCepLkQR2FZgLqRVHxQaY7LR3rJdbewdKY&part=snippet,statistics&chart=mostPopular"
+    //       )
+    //       .then((response) => {
+    //         const { data } = response;
+    //         setVideosData(data.items);
+    //       })
+    //       .catch((error) => {
+    //         console.log(error);
+    //       });
+    // }, [])
+  
+    
+  return (
+    <div className="max-w-[1440px] w-full mx-auto gap-12  flex flex-col flex-wrap align-left justify-start my-20">
+      <h3 className="text-[32px]">Tutoriales</h3>
+        {/* {videosData.length > 0 ? <VideoList videosData={videosData}></VideoList> : null} */}
+        <VideoList></VideoList>
+    </div>
+  );
+}
+
+export default Tutorials
