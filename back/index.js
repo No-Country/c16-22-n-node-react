0ea0cf99e101615f9f3e04e25b8966daf010de8e
Cristina <clmbjcm@gmail.com>
2024-02-24 09:45:26 -0300

    fixed publicDirVercel

--- back/index.js
+++ back/index.js
@@ -7,7 +7,7 @@ const { Server } = require("socket.io");
 // const fileUpload = require("express-fileupload");
 const { cloudinaryConfig } = require('./config/cloudinary');
 const publicDir = path.resolve(process.cwd(), "var/task/public");
-const publicDirVercel = path.resolve(process.cwd(), "var/task/public");
+const publicDirVercel = path.resolve(process.cwd(), "/public");
 console.log('-------local----------')
 console.log(process.cwd())
 console.log(publicDir)
