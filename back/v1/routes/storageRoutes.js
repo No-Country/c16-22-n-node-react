b9dc2f409dc4e78ff1953231814fabf57305b218
Cristina <clmbjcm@gmail.com>
2024-02-25 18:59:20 -0300

    add limit memory functions vercel

--- back/v1/routes/storageRoutes.js
+++ back/v1/routes/storageRoutes.js
@@ -10,17 +10,17 @@ router.post('/', async (req, res) => {
         console.log('-------------file.buffer--------------------');
         console.log(file.data);
 
-        // const stream = await cloudinary.uploader.upload_stream(
-        //     {
-        //         folder: 'serviya',
-        //     },
-        //     (error, result) => {
-        //         if (error) return console.error(error);
-        //         res.status(200).json(result);
-        //     }
-        // );
+        const stream = await cloudinary.uploader.upload_stream(
+            {
+                folder: 'serviya',
+            },
+            (error, result) => {
+                if (error) return console.error(error);
+                res.status(200).json(result);
+            }
+        );
 
-        // streamifier.createReadStream(file.data).pipe(stream);
+        streamifier.createReadStream(file.data).pipe(stream);
     } catch (error) {
         console.error(error);
         res.status(500).json({ message: "Error al subir el archivo" });
