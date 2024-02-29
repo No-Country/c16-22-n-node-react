de5ef2df43985fead2e8670816f177ef40e06ae8
Cristina <clmbjcm@gmail.com>
2024-02-29 11:00:51 -0300

    initial professional

---
+++ front/src/pages/Professional.jsx
@@ -0,0 +1,15 @@
+
+import Footer from "../components/footer/Footer";
+import Nav from "../components/nabvar/Navbar";
+
+const Professional = () => {
+    return (
+        <div>
+            <Nav />
+            Professional
+            <Footer />
+        </div>
+    )
+}
+
+export default Professional;
--- front/src/router/index.jsx
+++ front/src/router/index.jsx
@@ -1,26 +1,31 @@
 import { createBrowserRouter } from "react-router-dom";
 import Home from "../pages/Home";
 import Chat from '../pages/Chat'
+import Professional from "../pages/Professional";
 
 
-export const router = createBrowserRouter ([
-     //  ubicar rutas aqui ejemplo:
+export const router = createBrowserRouter([
+  //  ubicar rutas aqui ejemplo:
 
-     {
-          path: "/",
-          element: <Home />,
-      },
-      {
-        path: "/chat",
-        element: <Chat/>
-      }
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/chat",
+    element: <Chat />
+  },
+  {
+    path: "/professional",
+    element: <Professional />
+  }
 
-    // {
-    //     path: "/about",
-    //     element: <About />,
-    // },
-    // {
-    //     path: "/blog",
-    //     element: <Blog />,
-    // },
+  // {
+  //     path: "/about",
+  //     element: <About />,
+  // },
+  // {
+  //     path: "/blog",
+  //     element: <Blog />,
+  // },
 ])
