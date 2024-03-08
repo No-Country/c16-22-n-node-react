9cfceef533edf03b27dc038ed6ac547d00e0557f
Cristina <clmbjcm@gmail.com>
2024-03-08 17:45:10 -0300

    expand size card reserv and payment

--- front/src/components/mapview/MapView.jsx
+++ front/src/components/mapview/MapView.jsx
@@ -1,20 +1,24 @@
-import { MapContainer, TileLayer, Marker, Popup, useMap } from 'react-leaflet'
+import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
+
 import s from './MapView.module.css'
 
 const Mapview = () => {
+    const center = [51.505, -0.09]; // Coordenadas del centro del mapa
+    const zoom = 13;
+
     return (
         <div className={s.map}>
-            <MapContainer center={[-37.60288, -62.40207]} zoom={7} scrollWheelZoom={false}>
+            <Map center={center} zoom={zoom}>
                 <TileLayer
                     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 />
-                <Marker position={[51.505, -0.09]}>
+                <Marker position={center}>
                     <Popup>
-                        A pretty CSS3 popup. <br /> Easily customizable.
+                        Londres, Reino Unido
                     </Popup>
                 </Marker>
-            </MapContainer>
+            </Map>
         </div>
     )
 }
