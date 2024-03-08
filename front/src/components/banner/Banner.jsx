20c1091f5d45bd7e1d449ee959e27ef8abee2a26
Cristina <clmbjcm@gmail.com>
2024-03-08 09:01:37 -0300

    add button reserva

--- front/src/components/banner/Banner.jsx
+++ front/src/components/banner/Banner.jsx
@@ -1,11 +1,21 @@
 
 import { useEffect, useState } from "react";
+import { useNavigate } from 'react-router-dom';
+import useStoreProfessional from "../../store/useStoreProfessional";
 import s from "./Banner.module.css"
 import axios from 'axios';
 
 const Banner = ({ profess }) => {
 
     const [prof, setProf] = useState([]);
+    const navigate = useNavigate();
+    const updateProfessional = useStoreProfessional(state => state.updateProfessional);
+
+    const reservar = (e) => {
+        e.preventDefault();
+        updateProfessional(prof)
+        navigate(`/reservar/${prof?._id}`)
+    }
 
     useEffect(() => {
         setProf(profess[0]);
@@ -15,10 +25,10 @@ const Banner = ({ profess }) => {
         <div className={s.banner}>
             <div className={s.cardProfessional}>
                 <div className={s.photo}>
-                    <img className={s.imgPhoto} src={prof.pic} alt="" />
+                    <img className={s.imgPhoto} src={prof?.pic} alt="" />
                 </div>
                 <div className={s.datos}>
-                    <div className={s.name}>{prof.name} {prof.lastName}</div>
+                    <div className={s.name}>{prof?.name} {prof?.lastName}</div>
                     <div className={s.value}>
                         <div className={s.stars}>
                             <img className={s.star} src="/banner/estrella.svg" alt="" />
@@ -30,7 +40,7 @@ const Banner = ({ profess }) => {
                         <div className={s.textValue}>({prof?.comments?.length} valoraciones)</div>
                     </div>
 
-                    <button className={s.reservaButton}>Reservar consulta</button>
+                    <button onClick={(e) => reservar(e)} className={s.reservaButton}>Reservar consulta</button>
 
                 </div>
             </div>
@@ -40,7 +50,7 @@ const Banner = ({ profess }) => {
                     <div className={s.vectorCircle}><img className={s.vector} src="/banner/vector.svg" alt="" /></div>
                 </div>
 
-                <div className={s.consultPrice}>Valor de consulta: {prof.consultPrice} ARS</div>
+                <div className={s.consultPrice}>Valor de consulta: {prof?.consultPrice} ARS</div>
             </div>
         </div>
     )
